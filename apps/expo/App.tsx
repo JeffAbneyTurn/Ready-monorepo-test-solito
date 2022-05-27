962b1287d0ff98d940707205232f4b18a5d00786
Jeff Abney <jeffmabney@gmail.com>
2022-05-27 14:05:43 -0500

    Remove Tailwind provider from previous version

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,15 +1,11 @@
 import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
 import React from 'react';
-import {TailwindProvider} from 'tailwind-rn';
-import utilities from '../../tailwind.json';
 
 export default function App() {
   return (
     <Provider>
-      <TailwindProvider utilities={utilities}>
       <NativeNavigation />
-      </TailwindProvider>
     </Provider>
   )
 }
