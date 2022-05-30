6d1f1051581686b0ffbd9839e05f9c08305c71a4
Jeff Abney <jeffmabney@gmail.com>
2022-05-30 16:13:57 -0500

    testing packages/app placement of tailwind

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  corePlugins: { transform: false, translate: false, boxShadow: false },
-  content: ['./packages/app/features/**/*.tsx'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
