6d01797a23467bd9090b316d857be1b08c3fdcbf
Jeff Abney <jeffmabney@gmail.com>
2022-05-30 15:50:44 -0500

    testing changes in tailwind styles
    merging from main

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,16 +1,15 @@
 import { Dripsy } from './dripsy'
 import { NavigationProvider } from './navigation'
-import React from 'react';
-import { TailwindProvider } from 'tailwind-rn/dist';
-import utilities from '../tailwind.json';
-
+import React from 'react'
+import { TailwindProvider } from 'tailwind-rn/dist'
+import utilities from '../tailwind.json'
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <NavigationProvider>
       <TailwindProvider utilities={utilities}>
         <Dripsy>{children}</Dripsy>
-        </TailwindProvider>
+      </TailwindProvider>
     </NavigationProvider>
   )
 }
